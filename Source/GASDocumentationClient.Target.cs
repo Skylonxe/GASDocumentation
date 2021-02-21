0381d333f13435f70a19a86d05ec68f9ee33e3fc
Ondrej Hrusovsky <ondrej.hrus@gmail.com>
2021-02-22 00:11:39 +0100

    Update GASDocumentationClient.Target.cs

--- Source/GASDocumentationClient.Target.cs
+++ Source/GASDocumentationClient.Target.cs
@@ -1,9 +1,9 @@
 using UnrealBuildTool;
 using System.Collections.Generic;
 
-public class GASDocumentationTarget : TargetRules
+public class GASDocumentationClientTarget : TargetRules
 {
-	public GASDocumentationTarget(TargetInfo Target) : base(Target)
+	public GASDocumentationClientTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Client;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
