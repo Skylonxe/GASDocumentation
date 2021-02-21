2c340f8f1232b9232d43f47c18cdb1aedc303a7f
Ondrej Hrusovsky <ondrej.hrus@gmail.com>
2021-02-22 00:12:11 +0100

    GASDocumentationServer.Target.cs

---
+++ Source/GASDocumentationServer.Target.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class GASDocumentationServerTarget : TargetRules
+{
+	public GASDocumentationServerTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Server;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("GASDocumentation");
+	}
+}
